3637606518787b5ba50063d3db4b67c4adb76688
Abdukarim <abdukarimuz@mail.ru>
2022-08-19 14:19:45 +0500

    complected

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        for (int i = 65; i <= 90; i++){
+            System.out.println((char) i);
+        }
     }
+
+
 }
--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        int sum = 0, r;
+        if (t < 0){
+            t = t * -1;
+        }
+        while (t > 0){
+            r = t %10;
+            sum+=r;
+            t = t / 10;
+        }
+        System.out.println(sum);
     }
+
+
 }
